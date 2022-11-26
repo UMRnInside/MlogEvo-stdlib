0ff5e35f5a1386a17a76d9edd26c78991a69ea00
UMRnInside <30196401+UMRnInside@users.noreply.github.com>
2022-11-26 11:43:41 +0800

    Add everything.h

---
+++ include/mlogevo/everything.h
@@ -0,0 +1,12 @@
+#ifndef _MLOGEVO_EVERYTHING_H
+#define _MLOGEVO_EVERYTHING_H
+
+#include <mlogevo/arch.h>
+#include <mlogevo/block_control.h>
+#include <mlogevo/io.h>
+#include <mlogevo/math.h>
+#include <mlogevo/mem.h>
+#include <mlogevo/radar.h>
+#include <mlogevo/unit.h>
+
+#endif // _MLOGEVO_EVERYTHING_H
